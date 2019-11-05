0a71f3673333d22f3818278488e3fc69c16fc583
CoderTag <sarkar.kaushik.2000@gmail.com>
2019-11-05 20:29:04 +0530

    changed sidebar and add temporary folder

--- blog/static/blog/js/sidebar.js
+++ blog/static/blog/js/sidebar.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-
-    $('.sidebarCollapse').on('click', function () {
-        $('#sidebar,#content').toggleClass('active');
-        
-    });
+    $(window).resize(function () {
+        if ($(window).width() < 768) {
+            $('.sidebarCollapse').on('click', function () {
+                $("#sidebar").toggleClass('active');        
+            });           
+        }
+        else{
+            $('#sidebar').on('mouseenter', function () {
+                $('#sidebar,#content').removeClass('active');        
+            });
+            $('#sidebar').on('mouseleave', function () {
+                $('#sidebar,#content').addClass('active');        
+            });
+        }
+    }).resize(); //to initialize the value
 
 });
---
+++ temp_folder/Python/1.py
@@ -0,0 +1,6 @@
+ar = [1,2,4,5]
+for a in ar:
+    print(a)
+
+msg = "help342"
+print(msg.capitalize())
